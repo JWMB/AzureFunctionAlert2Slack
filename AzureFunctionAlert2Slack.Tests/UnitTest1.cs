58b13e5d8ae621ee8379e39ddf661fa1fdd0d1fc
JWMB <jonas.beckeman@gmail.com>
2022-12-01 14:34:41 +0100

    typed config, unit test

--- AzureFunctionAlert2Slack.Tests/UnitTest1.cs
+++ AzureFunctionAlert2Slack.Tests/UnitTest1.cs
@@ -1,30 +1,47 @@
-using AzureMonitorAlertToSlack.Services.Implementations;
 using AzureMonitorAlertToSlack.Services;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Xunit;
 using Shouldly;
-using Microsoft.Extensions.Logging;
-using AutoFixture;
+using AzureMonitorAlertToSlack;
+using AzureMonitorAlertToSlack.Services.LogQuery;
 
 namespace AzureFunctionAlert2Slack.Tests
 {
     public class UnitTest1
     {
         [Theory]
-        [InlineData(true)]
         [InlineData(false)]
+        [InlineData(true)]
         public void Services_UseLogQueryService(bool useLogQueryService)
         {
             var services = new ServiceCollection();
 
+            var appSettings = new AppSettings
+            {
+                LogQuery = new LogQuerySettings
+                {
+                    Enabled = useLogQueryService,
+                    ApplicationInsights = new ApplicationInsightsQuerySettings
+                    {
+                        ApiKey = "1",
+                        AppId = "1",
+                    },
+                    LogAnalytics = new LogAnalyticsQuerySettings
+                    {
+                        WorkspaceId = "1"
+                    }
+                },
+                Slack = new AzureMonitorAlertToSlack.Services.Slack.SlackSettings
+                {
+                    DefaultWebhook = "1"
+                }
+            };
+            var flatDict = ObjectToFlatDictionary(appSettings, "AppSettings").ToDictionary(o => o.key, o => o.value);
+
             var config = new ConfigurationBuilder()
-                .AddInMemoryCollection(new Dictionary<string, string> {
-                    { "UseLogQueryService", useLogQueryService ? "1" : "" },
-                    { "workspaceId", "1" },
-                    { "ApplicationInsightsAppId", "1" },
-                    { "ApplicationInsightsApiKey", "1" },
-                }).Build();
+                .AddInMemoryCollection(flatDict)
+                .Build();
 
             services.AddSingleton<IConfiguration>(config);
 
@@ -32,10 +49,41 @@ namespace AzureFunctionAlert2Slack.Tests
 
             var sp = services.BuildServiceProvider();
 
+            ShouldOrShouldNotThrow(useLogQueryService == false, () => sp.GetRequiredService<ILogAnalyticsQueryService>());
+
+            Should.NotThrow(sp.GetRequiredService<ApplicationInsightsQuerySettings>);
+            Should.NotThrow(sp.GetRequiredService<LogAnalyticsQuerySettings>);
             Should.NotThrow(sp.GetRequiredService<IMessageSender>);
             Should.NotThrow(sp.GetRequiredService<IAlertInfoFactory>);
 
             Should.NotThrow(sp.GetRequiredService<RequestToSlackFunction>);
+
+            void ShouldOrShouldNotThrow(bool shouldThrow, Action action)
+            {
+                if (shouldThrow)
+                    Should.Throw<Exception>(action);
+                else
+                    Should.NotThrow(action);
+            }
+        }
+
+        public static IEnumerable<(string key, string value)> ObjectToFlatDictionary(object obj, string path = "")
+        {
+            var type = obj.GetType();
+            var properties = type.GetProperties(System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance).Where(o => o.CanWrite);
+            foreach (var prop in properties)
+            {
+                var fullPath = $"{(path.Any() ? $"{path}:" : "")}{prop.Name}";
+                var val = prop.GetValue(obj);
+                if (val == null)
+                    ; // yield return (fullPath, "");
+                else if (val.GetType().IsClass && val is not string)
+                    foreach (var item in ObjectToFlatDictionary(val, fullPath))
+                        yield return item;
+                else
+                    yield return (fullPath, val.ToString() ?? "");
+
+            }
         }
     }
 }
--- AzureFunctionAlert2Slack/TypedConfiguration.cs
+++ AzureFunctionAlert2Slack/TypedConfiguration.cs
@@ -2,7 +2,9 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Reflection;
 
 namespace AzureFunctionAlert2Slack
 {
@@ -11,72 +13,70 @@ namespace AzureFunctionAlert2Slack
         public static T ConfigureTypedConfiguration<T>(IServiceCollection services, IConfiguration config, string sectionName, bool throwOnMissing = true)
             where T : new()
         {
-            // TODO: (low) continue investigation - how to avoid reflection and get validation errors immediately
-
-            // Note: This does NOT cause validation on startup...: services.AddOptions<AceKnowledgeConfiguration>().Bind(config.GetSection("AceKnowledge")).ValidateDataAnnotations().ValidateOnStart();
+            // TODO: continue investigation - how to avoid reflection and get validation errors immediately
+            // Note: The following does NOT cause validation on startup...: services.AddOptions<AceKnowledgeConfiguration>().Bind(config.GetSection("AceKnowledge")).ValidateDataAnnotations().ValidateOnStart();
 
             // https://referbruv.com/blog/posts/working-with-options-pattern-in-aspnet-core-the-complete-guide
             var appSettings = new T();
 
             config.GetSection(sectionName).Bind(appSettings);
-            services.AddSingleton(appSettings.GetType(), appSettings!);
+            // TODO: we should recursively check for missing sections/values (where the property is not nullable)
+            //services.AddSingleton(appSettings.GetType(), appSettings!);
 
-            var props = appSettings.GetType()
-                .GetProperties(System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Public)
-                .Where(o => !o.PropertyType.IsSealed); // TODO: (low) better check than IsSealed (also unit test)
-            foreach (var prop in props)
-            {
-                var instance = prop.GetValue(appSettings);
-                if (instance == null)
-                {
-                    if (throwOnMissing)
-                        throw new NullReferenceException($"{prop.Name} does not exist");
-                }
-                else
-                {
-                    //config.GetSection(prop.Name).Bind(instance);
-                    services.AddSingleton(instance.GetType(), instance!);
-
-                    //var asOptions = Microsoft.Extensions.Options.Options.Create(instance);
-                    //services.ConfigureOptions(instance);
-
-                    // Execute validation (if available)
-                    var validatorType = instance.GetType().Assembly.GetTypes()
-                       .Where(t =>
-                       {
-                           var validatorInterface = t.GetInterfaces().SingleOrDefault(o =>
-                           o.IsGenericType && o.GetGenericTypeDefinition() == typeof(Microsoft.Extensions.Options.IValidateOptions<>));
-                           return validatorInterface != null && validatorInterface.GenericTypeArguments.Single() == instance.GetType();
-                       }).FirstOrDefault();
-
-                    if (validatorType != null)
-                    {
-                        var validator = Activator.CreateInstance(validatorType);
-                        var m = validatorType.GetMethod("Validate");
-                        var result = (Microsoft.Extensions.Options.ValidateOptionsResult?)m?.Invoke(validator, new object[] { "", instance });
-                        if (result!.Failed)
-                        {
-                            throw new Exception($"{validatorType.Name}: {result.FailureMessage}");
-                        }
-                    }
-                }
-            }
+            RecurseRegister(new { Root = appSettings }, services);
 
             return appSettings;
             // https://kaylumah.nl/2021/11/29/validated-strongly-typed-ioptions.html
             // If we want to inject IOptions<Type> instead of just Type, this is needed: https://stackoverflow.com/a/61157181 services.ConfigureOptions(instance)
-            //services.Configure<AceKnowledgeOptions>(config.GetSection("AceKnowledge"));
         }
-        //static void ConfigureAppConfiguration(IConfigurationBuilder configBuilder, IHostEnvironment env)
-        //{
-        //    // CreateDefaultBuilder messes up providers, doing it manually: https://github.com/dotnet/aspnetcore/issues/19924
-        //    configBuilder.Sources.Clear();
-        //    configBuilder.SetBasePath(Directory.GetCurrentDirectory());
-        //    configBuilder.AddJsonFile("appsettings.json");
-        //    configBuilder.AddJsonFile($"appsettings.{env.EnvironmentName}.json", true);
-        //    configBuilder.AddUserSecrets<Program>();
-        //    configBuilder.AddEnvironmentVariables();
-        //}
+
+        private static void RecurseRegister(object obj, IServiceCollection services)
+        {
+            var props = obj.GetType()
+                .GetProperties(BindingFlags.Instance | BindingFlags.Public)
+                .Where(o => !o.PropertyType.IsSealed) // TODO: (low) better check than IsSealed (also unit test)
+                .Where(o => o.PropertyType.IsClass && o.PropertyType != typeof(string));
+
+            foreach (var prop in props)
+            {
+                var instance = prop.GetValue(obj);
+                if (instance == null)
+                {
+                    var nullabilityInfo = new NullabilityInfoContext().Create(prop);
+                    if (nullabilityInfo.WriteState is not NullabilityState.Nullable)
+                        throw new NullReferenceException($"{nameof(prop.Name)} is null"); // should not be possible
+                }
+                else
+                {
+                    // Validate(instance);
+                    services.AddSingleton(prop.PropertyType, instance);
+                    RecurseRegister(instance, services);
+                }
+            }
+        }
+
+        private static void Validate(object instance)
+        {
+            // Execute validation (if available)
+            var validatorType = instance.GetType().Assembly.GetTypes()
+               .Where(t =>
+               {
+                   var validatorInterface = t.GetInterfaces().SingleOrDefault(o =>
+                   o.IsGenericType && o.GetGenericTypeDefinition() == typeof(Microsoft.Extensions.Options.IValidateOptions<>));
+                   return validatorInterface != null && validatorInterface.GenericTypeArguments.Single() == instance.GetType();
+               }).FirstOrDefault();
+
+            if (validatorType != null)
+            {
+                var validator = Activator.CreateInstance(validatorType);
+                var m = validatorType.GetMethod("Validate");
+                var result = (Microsoft.Extensions.Options.ValidateOptionsResult?)m?.Invoke(validator, new object[] { "", instance });
+                if (result!.Failed)
+                {
+                    throw new Exception($"{validatorType.Name}: {result.FailureMessage}");
+                }
+            }
+        }
     }
 
 }
