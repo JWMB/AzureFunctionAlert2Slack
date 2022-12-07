25a0806654081b35efe69b148653f46a6861cbab
JWMB <jonas.beckeman@gmail.com>
2022-12-07 11:48:49 +0100

    QueryAI: use CustomProperties querySuffix

--- AzureFunctionAlert2Slack.Tests/UnitTest1.cs
+++ AzureFunctionAlert2Slack.Tests/UnitTest1.cs
@@ -8,6 +8,9 @@ using AzureMonitorAlertToSlack.Alerts;
 using AzureMonitorCommonAlertSchemaTypes.AlertContexts;
 using AzureMonitorCommonAlertSchemaTypes;
 using AzureMonitorCommonAlertSchemaTypes.AlertContexts.LogAlertsV2;
+using AutoFixture;
+using Moq;
+using AutoFixture.AutoMoq;
 
 namespace AzureFunctionAlert2Slack.Tests
 {
@@ -91,6 +94,38 @@ namespace AzureFunctionAlert2Slack.Tests
             alertHandler.LogAlertsV2AlertContext(alert, ctx);
 
             alertHandler.Handled.Title.ShouldBe($"{alert.Data.CustomProperties["titlePrefix"]}{alert.Data.CustomProperties["titleSuffix"]}");
+        }
+
+        [Fact]
+        public void MyDemuxedAlertHandler_QuerySuffix()
+        {
+            var fixture = new Fixture().Customize(new AutoMoqCustomization());
+            var qService = fixture.Create<ILogQueryService>();
+
+            //var qFactory = fixture.Build<ILogQueryServiceFactory>()
+            //    .With(o => o.CreateLogQueryService(It.IsAny<string>()), () => qService)
+            //    .Create();
+            var mQFactory = new Mock<ILogQueryServiceFactory>();
+            mQFactory.Setup(o => o.CreateLogQueryService(It.IsAny<string>())).Returns(qService);
+
+            var alertHandler = new MyDemuxedAlertHandler(mQFactory.Object);
+
+            var alert = new Alert();
+            var ctx = new LogAlertsV2AlertContext();
+            var criteria = new LogQueryCriteria[] { new LogQueryCriteria { SearchQuery = "AppTraces" } };
+            ctx.Condition.AllOf = criteria;
+            alert.Data.AlertContext = ctx;
+
+            alert.Data.CustomProperties = new Dictionary<string, string>
+            {
+                { "querySuffix", "| project Timestamp"},
+            };
+
+            alertHandler.LogAlertsV2AlertContext(alert, ctx, criteria);
+
+            var expectedQuery = $"{criteria.Single().SearchQuery}\n{alert.Data.CustomProperties["querySuffix"]}";
+            Mock.Get(qService).Verify(o => o.GetQueryAsDataTable(It.Is<string>(o => o == expectedQuery), It.IsAny<DateTimeOffset>(), It.IsAny<DateTimeOffset>(), It.IsAny<CancellationToken?>()),
+                Times.Once);
         }
     }
 }
--- AzureFunctionAlert2Slack/MyDemuxedAlertHandler.cs
+++ AzureFunctionAlert2Slack/MyDemuxedAlertHandler.cs
@@ -3,7 +3,9 @@ using AzureMonitorAlertToSlack.LogQuery;
 using AzureMonitorCommonAlertSchemaTypes;
 using AzureMonitorCommonAlertSchemaTypes.AlertContexts;
 using AzureMonitorCommonAlertSchemaTypes.AlertContexts.LogAlertsV2;
+using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace AzureFunctionAlert2Slack
 {
@@ -15,15 +17,23 @@ namespace AzureFunctionAlert2Slack
 
         protected override void SetHandled(SummarizedAlert item)
         {
-            item.Title = $"{GetCustomProperty("titlePrefix")}{item.Title}{GetCustomProperty("titleSuffix")}";
+            item.Title = $"{GetCustomProperty(item, "titlePrefix")}{item.Title}{GetCustomProperty(item, "titleSuffix")}";
             base.SetHandled(item);
-
-            string GetCustomProperty(string key, string defaultValue = "") =>
-                item.CustomProperties?.GetValueOrDefault(key, defaultValue) ?? defaultValue;
         }
         public override void LogAlertsV2AlertContext(Alert alert, LogAlertsV2AlertContext ctx, DynamicThresholdCriteria[] criteria)
         {
             base.LogAlertsV2AlertContext(alert, ctx, criteria);
         }
+
+        protected override Task<string?> QueryAI(SummarizedAlert handled, string targetResourceTypes, string? query, DateTimeOffset start, DateTimeOffset end)
+        {
+            var querySuffix = GetCustomProperty(handled, "querySuffix");
+            query = $"{query}{(string.IsNullOrEmpty(querySuffix) ? "" : $"\n{querySuffix}")}";
+            return base.QueryAI(handled, targetResourceTypes, query, start, end);
+        }
+
+        private string GetCustomProperty(SummarizedAlert item, string key, string defaultValue = "") =>
+                item.CustomProperties?.GetValueOrDefault(key, defaultValue) ?? defaultValue;
+
     }
 }
